39a52328c8705e6fff215d9853d1d42cc5096586
aaravtheleader <74689040+aaravtheleader@users.noreply.github.com>
2021-02-09 20:17:54 -0500

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBKVOW-MWn7CQLxUyMvE55u4cdWRLOzrjk",
+    databaseURL:"https://kwitter-b5316-default-rtdb.firebaseio.com/",
+    authDomain: "kwitter-b5316.firebaseapp.com",
+    projectId: "kwitter-b5316",
+    storageBucket: "kwitter-b5316.appspot.com",
+    messagingSenderId: "297412377172",
+    appId: "1:297412377172:web:bff93ebdf46a15ffabcbc1"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
